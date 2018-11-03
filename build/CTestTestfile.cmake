9327019bc82af22b0ed322193849d93b33bead10
ChunChieh,Wang <w.c.c.8511@gmail.com>
2018-11-03 13:08:11 +0800

    First Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/jerrywang/MotorSim/src
+# Build directory: /home/jerrywang/MotorSim/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(MotorSim_description)
+subdirs(MotorSim_gazebo)
+subdirs(MotorSim_control)
---
+++ build/MotorSim_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jerrywang/MotorSim/src/MotorSim_description
+# Build directory: /home/jerrywang/MotorSim/build/MotorSim_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
