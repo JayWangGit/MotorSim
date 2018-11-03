24ca4f8040a32032a22c0eb5fb0c7720d45b7dd6
ChunChieh,Wang <w.c.c.8511@gmail.com>
2018-11-04 01:26:41 +0800

    Update

---
+++ build/motorsim_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jerrywang/MotorSim/src/motorsim_control
+# Build directory: /home/jerrywang/MotorSim/build/motorsim_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/motorsim_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jerrywang/MotorSim/src/motorsim_description
+# Build directory: /home/jerrywang/MotorSim/build/motorsim_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
